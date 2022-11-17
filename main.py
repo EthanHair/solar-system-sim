0111f5b37b4bb6d4b7ab0d355701cfe53e7f5d32
Ethan Hair <ethan@astropanda.studio>
2022-11-16 21:03:51 -0600

    undo rename

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # This allows the file to be aware of the files in the Classes folder. Also the settings.json 
 # file in the .vscode folder does the same if you are using VSCode as your editor
 import sys
-sys.path.insert(0, ".\\classes")
+sys.path.insert(0, ".\\Classes")
 
 from planet import Planet
 from sun import Sun
